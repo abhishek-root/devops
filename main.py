7c217fb90d353bb73ae9bd2de7e337dadc22fa37
abhishek-root <905950ab@gmail.com>
2025-10-15 21:22:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+"""Fastapi application for basic health & root endpoints"""
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def root():
+    """Return a welcome mesage"""
     return {"message": "Hello World from FastAPI"}
 
 @app.get("/health")
 def health_check():
+    """return health status"""
     return {"status": "ok"}
-
-
