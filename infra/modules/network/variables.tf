84d560367d842b7c81ad8fccf2a5a68181e81b87
abhishek-root <905950ab@gmail.com>
2025-10-16 01:00:14 +0530

    UN

--- infra/modules/network/variables.tf
+++ infra/modules/network/variables.tf
@@ -1,4 +1,8 @@
 variable "resource_group_name" {}
 variable "location" {}
-variable "vnet_name" {}
-variable "subnet_name" {}
+variable "vnet_name" {
+    default = "devops-vnet-test"
+}
+variable "subnet_name" {
+    default = "devops-subnet"
+}
