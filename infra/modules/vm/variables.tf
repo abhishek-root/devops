20609d992f25dcc6047752e7af72bcffefae0bb2
abhishek-root <905950ab@gmail.com>
2025-10-24 11:54:32 +0530

    update tf variables

--- infra/modules/vm/variables.tf
+++ infra/modules/vm/variables.tf
@@ -2,4 +2,10 @@ variable "resource_group_name" {}
 variable "location" {}
 variable "subnet_id" {}
 variable "vm_name" {}
+variable "admin_password" {
+  type      = string
+  sensitive = true
+  description = "Admin password for the VM"
+}
+
 
